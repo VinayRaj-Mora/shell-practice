30bc2e57464fefc0a57b1d737d969480f4bc74e9
vinay raj <vinaymudhiraj918@gmail.com>
2025-11-26 17:41:53 +0530

    some meaage

---
+++ 04-varibales.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+echo "$person1:: welcome to Alaska $person2"
+echo "$person2:: thank you $person1"
+echo "$person1:: how is the weather there?"
+echo "$person2:: its very cold here"
