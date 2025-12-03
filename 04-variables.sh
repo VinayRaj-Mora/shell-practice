1dbf6a10074a90f6bc723ea3781ef1bef26a095a
vinay raj <vinaymudhiraj918@gmail.com>
2025-12-03 15:41:44 +0530

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+Person1=$1 # Assign the name "Trump" to the variable Person1
+Person2=$2
+  # Assign the name "Putin" to the variable Person
+echo "$Person1:: welcome to the jungle!"  # Print a welcome message
+echo "$Person2:: we need to talk."        # Print another message
+echo "$Person1:: let's make a deal."    # Print a third message
