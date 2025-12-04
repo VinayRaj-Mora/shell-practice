58d5c49d23fb172d9417e073572196638c25e4a0
vinay raj <vinaymudhiraj918@gmail.com>
2025-12-04 11:03:00 +0530

    variables

---
+++ 12-installation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+# This script installs a list of packages using apt-get.
+
+USERID=$(id -u)
+
+IF [ $USERID -NE 0 ]; then
+    echo "Error:: Please run script with root priveleges"
+fi
+
+dnf install mysql -y
+
+if [$? -ne 0 ]; then
+    echo "Error:: Installation failed"
+    exit 1
+else
+    echo "Installation successful"
+fi
